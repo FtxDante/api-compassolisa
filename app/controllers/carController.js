2392333ff22847680470158c61b4a11258a06fb6
Natasha Milhomem <78516721+natashamilhomem@users.noreply.github.com>
2021-10-22 09:51:25 -0300

    some eslint formatting

--- app/controllers/carController.js
+++ app/controllers/carController.js
@@ -1,16 +1,15 @@
-const carService = require('../service/carService')
-
+/* eslint-disable require-jsdoc */
+const carService = require('../service/carService');
 class CarController {
-    static async createCar(req, res){
-        const result = await carService.create(req.body);
-        return res.status(201).json(result);
-    }
+  static async createCar(req, res) {
+    const result = await carService.create(req.body);
+    return res.status(201).json(result);
+  }
 
-    static async updateById(req, res) {
-        const result = await carService.updateById(req.body);
-        return res.status(201).json(result);
-    }
-
+  static async updateById(req, res) {
+    const result = await carService.updateById(req.body);
+    return res.status(201).json(result);
+  }
 }
 
 module.exports = CarController;
