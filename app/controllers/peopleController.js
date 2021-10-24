d98ede209c62285d65b7918766dd6a2e5778d888
evaristo <gabrielevaristovcp@gmail.com>
2021-10-24 11:05:15 -0300

    get all peoples is running

--- app/controllers/peopleController.js
+++ app/controllers/peopleController.js
@@ -1,11 +1,14 @@
-const peopleService = require('../service/peopleService')
+const peopleService = require("../service/peopleService");
 
-class PeopleController{
-    static async createPeople(req, res){
-        const result = await peopleService.createPeople(req.body);
-        return res.status(201).json(result);
-    }
-
+class PeopleController {
+  static async createPeople(req, res) {
+    const result = await peopleService.createPeople(req.body);
+    return res.status(201).json(result);
+  }
+  static async getAllPeople(req, res) {
+    const result = await peopleService.findAll(req, res);
+    return res.status(201).json(result);
+  }
 }
 
 module.exports = PeopleController;
--- app/repository/peopleRepository.js
+++ app/repository/peopleRepository.js
@@ -1,10 +1,36 @@
-const peopleSchema = require('../schema/peopleSchema');
+const PeopleSchema = require("../schema/peopleSchema");
 
-class PeopleRepository{
-    async create(peopleData){
-        return await peopleSchema.create(peopleData);
+class PeopleRepository {
+  async create(peopleData) {
+    return await PeopleSchema.create(peopleData);
+  }
+  async findAll(where = {}, page = null, limit = null) {
+    return await PeopleSchema.find(where)
+      .skip(page * limit)
+      .limit(limit);
+  }
+
+  async pagination(req, where = {}) {
+    try {
+      let { page = 0, limit = 100 } = req.query;
+
+      if (page > 0) page -= 1;
+
+      const data = await this.findAll(where, Number(page), Number(limit));
+
+      const dataTotal = await this.findAll(where);
+
+      return {
+        pessoas: data,
+        total: dataTotal.length,
+        limit: Number(limit),
+        offset: page + 1,
+        offsets: Math.ceil(dataTotal.length / limit),
+      };
+    } catch (error) {
+      return error;
     }
-
+  }
 }
 
-module.exports = new PeopleRepository;
+module.exports = new PeopleRepository();
