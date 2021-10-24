df5f5e83cc3e4f8ae6c05f2285a8409c3f7030e4
FtxDante <blog.anutrof@gmail.com>
2021-10-24 12:40:02 -0300

    indentation to repository and schema people is working

--- app/schema/peopleSchema.js
+++ app/schema/peopleSchema.js
@@ -1,37 +1,38 @@
 const mongoose = require('mongoose');
 
+// eslint-disable-next-line new-cap
 const peopleSchema = mongoose.Schema({
 
-    nome: {
-        type: String,
-        required: true
-    },
+  nome: {
+    type: String,
+    required: true,
+  },
 
-    cpf: {
-        type: String,
-        required: true
-    },
+  cpf: {
+    type: String,
+    required: true,
+  },
 
-    data_nascimento:{
-        type: Date,
-        required: true    
-    },
+  data_nascimento: {
+    type: Date,
+    required: true,
+  },
 
-    email:{
-        type: String,
-        required: true
-    },
+  email: {
+    type: String,
+    required: true,
+  },
 
-    senha:{
-        type: String,
-        required: true,
-        minLength: 6
-    },
-    habilitado:{
-        type:String,
-        enum: ['sim', 'nao'],
-        required: true
-    }
+  senha: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  habilitado: {
+    type: String,
+    enum: ['sim', 'nao'],
+    required: true,
+  },
 });
 
 const People = mongoose.model('People', peopleSchema);
