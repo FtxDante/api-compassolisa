27e2774806c6c5afe98917bbabb2e1bc770fadd6
Natasha Milhomem <78516721+natashamilhomem@users.noreply.github.com>
2021-10-25 15:45:03 -0300

    added id validation on put route

--- app/validation/people/index.js
+++ app/validation/people/index.js
@@ -1,5 +1,7 @@
 const CreateValidation = require('./create');
+const IdValidation = require('./idValidation');
 
 module.exports = {
   CreatePeopleValidation: CreateValidation,
+  IdValidation: IdValidation,
 };
--- routes/peopleRoutes.js
+++ routes/peopleRoutes.js
@@ -1,10 +1,11 @@
 const PeopleController = require('../app/controllers/peopleController');
 const {CreatePeopleValidation} = require('../app/validation/people');
+const {IdValidation} = require('../app/validation/people');
 
 module.exports = (server, routes, prefix = '/api/v1') =>{
   routes.post('/people', CreatePeopleValidation, PeopleController.createPeople);
   routes.get('/people?', PeopleController.getAllPeople);
-  routes.put('/people/:id',
+  routes.put('/people/:id', IdValidation,
       CreatePeopleValidation,
       PeopleController.updateOnePerson);
   server.use(prefix, routes);
