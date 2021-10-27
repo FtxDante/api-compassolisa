6b6a78876127374f979d56c7662ea13e0f4df70e
Christoffer Cavalcante <christoffercavalcante@gmail.com>
2021-10-27 18:31:58 -0300

    Update index.js

--- app/validation/people/index.js
+++ app/validation/people/index.js
@@ -1,9 +1,7 @@
-const CreateValidation = require('./create');
 const DeletePeopleValidation = require('./deleteOne');
 const AllRequiredValidation = require('./allRequired');
 
 module.exports = {
-  CreatePeopleValidation: CreateValidation,
   DeletePeopleValidation: DeletePeopleValidation,
   AllRequiredValidation: AllRequiredValidation,
 };
