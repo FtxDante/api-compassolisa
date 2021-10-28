173e69d14f078562bd746424230fb3b6bbc5fc65
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-10-28 15:19:21 -0300

    Validations fix for People

--- app/validation/people/AllRequireValidation.js
+++ app/validation/people/AllRequireValidation.js
@@ -5,12 +5,29 @@ const Joi = require('joi').extend(validator);
 module.exports = async (req, res, next) =>{
   try {
     const peopleSchema = Joi.object({
-      nome: Joi.string().required(),
-      cpf: Joi.document().cpf().required(),
-      data_nascimento: Joi.date().required(),
-      email: Joi.string().email().required(),
-      senha: Joi.string().min(6).required(),
-      habilitado: Joi.string().valid('sim', 'nao').required(),
+      nome: Joi.string()
+          .required(),
+      cpf: Joi.document()
+          .cpf()
+          .required()
+          .custom((value, helper)=>{
+            const cpf2 = value.replace(/[^0-9]/g, '')
+                .replace(/(\d{3})?(\d{3})?(\d{3})?(\d{2})/, '$1.$2.$3-$4');
+            if (cpf2 !== value) {
+              return helper.message('Invalid CPF');
+            }
+          }),
+      data_nascimento: Joi.date()
+          .required(),
+      email: Joi.string()
+          .email()
+          .required(),
+      senha: Joi.string()
+          .min(6)
+          .required(),
+      habilitado: Joi.string()
+          .valid('sim', 'nao')
+          .required(),
 
     });
 
