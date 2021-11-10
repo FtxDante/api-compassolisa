a3f77bb51b6d9893b522f203eabb5dc2c2ba93c0
Natasha Milhomem <78516721+natashamilhomem@users.noreply.github.com>
2021-11-10 12:28:48 -0300

    added rental serializer

--- app/controllers/rentalController.js
+++ app/controllers/rentalController.js
@@ -1,11 +1,13 @@
-const rentalService = require('../service/rentalService');
 const { handleErrors } = require('../errors');
+const { rentalSerializer } = require('../serialize/RentalSerializer');
+const rentalService = require('../service/rentalService');
+
 class rentalController {
   static async getOneRental(req, res) {
     try {
       const { id } = req.params;
       const result = await rentalService.findById(id);
-      return res.status(200).json(result);
+      return res.status(200).json(rentalSerializer(result));
     } catch (error) {
       const status = handleErrors.getStatusToError(error);
       return res.status(status).json({ message: error.message });
