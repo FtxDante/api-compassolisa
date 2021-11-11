cbf367c7bc9cd7d0a266e55dcc76b5f2b3fc3ccf
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-11-11 19:12:10 -0300

    Initial JEST tests

--- infra/database/mongo/index.js
+++ infra/database/mongo/index.js
@@ -12,6 +12,7 @@ class Database {
   connect() {
     mongoose.Promise = global.Promise;
     console.log('MongoDB is running');
+
     return mongoose.connect(
       `mongodb:mongodb+srv://${config.user}:${config.pass}@${config.host}/${config.name}?retryWrites=true&w=majority`
     );
