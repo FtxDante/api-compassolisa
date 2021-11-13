48ca5cc4979f1e65797ce8f4a5a1cca69633a6d6
FtxDante <blog.anutrof@gmail.com>
2021-11-13 16:21:49 -0300

    jest test erros of get by id

--- __tests__/RentalController/getRental.test.js
+++ __tests__/RentalController/getRental.test.js
@@ -18,9 +18,26 @@ describe('Get /rental', () => {
     expect(body.endereco[0]).toHaveProperty('cep');
     expect(body.endereco[0]).toHaveProperty('number');
     expect(body.endereco[0]).toHaveProperty('isFilial');
+    expect(body.endereco[0].isFilial).toBe(false);
   });
 
-  test('Get a Rental with bad ID', async () => {
+  test('Get a Rental With Sucess, with filial', async () => {
+    const response = await request.get('/api/v1/rental/618c09496e34ed17186c9c5d');
+    const { body, status } = response;
+    expect(status).toBe(200);
+
+    expect(body).toHaveProperty('nome');
+    expect(body).toHaveProperty('cnpj');
+    expect(body).toHaveProperty('atividades');
+    expect(body).toHaveProperty('endereco');
+    expect(body.endereco[0]).toHaveProperty('cep');
+    expect(body.endereco[0]).toHaveProperty('number');
+    expect(body.endereco[0]).toHaveProperty('isFilial');
+
+    expect(body.endereco[0].isFilial).toBe(false);
+  });
+
+  test('Get a Rental with bad format ID', async () => {
     const response = await request.get('/api/v1/rental/u12832189e8912e');
     const { body, status } = response;
     expect(status).toBe(400);
@@ -29,5 +46,16 @@ describe('Get /rental', () => {
     expect(body[0]).toHaveProperty('name');
     expect(body[0].description).toBe('id');
     expect(body[0].name).toBe('invalid id format');
+  });
+
+  test('Get a Rental with id Not Found', async () => {
+    const response = await request.get('/api/v1/rental/618c09496f34ed17186c9d5d');
+    const { body, status } = response;
+    expect(status).toBe(404);
+
+    expect(body).toHaveProperty('description');
+    expect(body).toHaveProperty('name');
+    expect(body.description).toBe('NotFound');
+    expect(body.name).toBe('id not found');
   });
 });
