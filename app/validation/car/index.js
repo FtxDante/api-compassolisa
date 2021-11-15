fd10b17686917ddeeed34b9d7af029cbe59e92af
FtxDante <blog.anutrof@gmail.com>
2021-11-14 21:53:18 -0300

    removed find of validation, beacuseno one use that

--- app/validation/car/index.js
+++ app/validation/car/index.js
@@ -1,12 +1,10 @@
 const allRequiredCarValidation = require('./allRequired');
 
-const findCarValidation = require('./find');
 const idValidation = require('../__validationsCommons/idValidation');
 const pathValidation = require('./path');
 
 module.exports = {
   allRequiredCarValidation,
-  findCarValidation,
   idValidation,
   pathValidation
 };
